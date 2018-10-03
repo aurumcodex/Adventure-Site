0047df7cbe2af4f371d18fd1b46ad692512aa602
aurumcodex <syberfox.64@gmail.com>
2018-10-02 22:35:10 -0400

    added the setup for most html files; added some CSS rules

--- scripts/overarch.js
+++ scripts/overarch.js
@@ -2,3 +2,8 @@ function changeColor() {
     var element = document.getElementById("changeme");
     element.style.color = '#d73737';
 }
+
+function changePage() {
+    window.location = "journey/start.html";
+    // $(location).attr('href', 'http://stackoverflow.com');
+}
