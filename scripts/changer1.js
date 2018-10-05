88ce4eecd0b34b635a5ab0b0b64a9dc6807a0db6
aurumcodex <syberfox.64@gmail.com>
2018-10-04 20:22:46 -0400

    added a bunch of stuff; started on the quest chaining; etc.

--- scripts/changer1.js
+++ scripts/changer1.js
@@ -1,13 +1,30 @@
 $(document).ready(function () {
-    $('#clickme').click(function() {
-        $.ajax({url: "../naratives/test1.txt", success: function(result) {
-            $('#changeme').html(result);
-        }});
-    });
-
-    $('#clickme2').click(function() {
-        $('#changeme2').load("../naratives/button_test.txt");
-    });
-    
-    $('#autoload').load('../naratives/bones.html');
+    $('#autoload').load('../naratives/ranger_nar.html #init');
+    $('.button_menu').load('../naratives/ranger_nar.txt #init_menu');
+    $('.button_menu').on("click", "#charge_in", loadCharge);
+    $('.button_menu').on("click", "#hone_sense", clickMe);
 });
+
+function clickMe() { console.log("i've been clicked"); }
+
+function loadCharge() {
+    $('#autoload').load('../naratives/ranger_nar.html #charge');
+    $('.button_menu').load('../naratives/ranger_nar.html #charge_menu');
+    $('.button_menu').on("click", "#noise_ignore", resumeChrgPath);
+    $('.button_menu').on("click", "#noise_investigate", clickMe);
+}
+
+function resumeChrgPath() {
+    $('#autoload').load('../naratives/ranger_nar.html #resume_chrg_path');
+    $('.button_menu').load('../naratives/ranger_nar.html #resume_chrg_path_menu');
+    $('.button_menu').on("click", "#chrg_avoid", chrgAvoidCamp);
+    $('.button_menu').on("click", "#chrg_apprch", clickMe);
+}
+
+function chrgAvoidCamp() {
+    $('#autoload').load('../naratives/ranger_nar.html #chrg_cmpfr_avoid');
+    $('.button_menu').load('../naratives/ranger_nar.html #chrg_cmpfr_avoid_menu');
+    $('.button_menu').on("click", "#chrg_continue", toRngrBossPage);
+}
+
+function toRngrBossPage() { window.location.href = "./ranger_boss.html"; }
