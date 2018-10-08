0c6911d9cfab87934239e3792799e30c3827fbf3
aurumcodex <syberfox.64@gmail.com>
2018-10-08 15:45:53 -0400

    removed unnecessary files, changed names of the narrative files

--- scripts/scrpt_ranger.js
+++ scripts/scrpt_ranger.js
@@ -2,10 +2,10 @@ var get_bait = false;
 var get_treasure = false;
 
 $(document).ready(function () {
-    $('#autoload').load('../naratives/ranger_nar.html #init', function() {
+    $('#autoload').load('../narratives/ranger_nar.txt #init', function() {
         $(this).show(500);
     });
-    $('.button_menu').load('../naratives/ranger_nar.txt #init_menu', function() {
+    $('.button_menu').load('../narratives/ranger_nar.txt #init_menu', function() {
         $(this).show(500);
     });
     $('.button_menu').on("click", "#charge_in", loadCharge);
@@ -17,48 +17,48 @@ function clickMe() { console.log("i've been clicked"); }
 
 // charge path functions are denoted with "chrg"
 function loadCharge() {
-    $('#autoload').load('../naratives/ranger_nar.html #charge');
-    $('.button_menu').load('../naratives/ranger_nar.html #charge_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #charge');
+    $('.button_menu').load('../narratives/ranger_nar.txt #charge_menu');
     $('.button_menu').on("click", "#noise_ignore", chrgResumePath);
     $('.button_menu').on("click", "#noise_investigate", chrgInvestigate);
 }
 
 function chrgResumePath() {
-    $('#autoload').load('../naratives/ranger_nar.html #resume_chrg_path');
-    $('.button_menu').load('../naratives/ranger_nar.html #resume_chrg_path_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #resume_chrg_path');
+    $('.button_menu').load('../narratives/ranger_nar.txt #resume_chrg_path_menu');
     $('.button_menu').on("click", "#chrg_avoid", chrgAvoidCamp);
     $('.button_menu').on("click", "#chrg_apprch", chrgApprchCamp);
 }
 
 function chrgInvestigate() {
-    $('#autoload').load('../naratives/ranger_nar.html #investigation');
-    $('.button_menu').load('../naratives/ranger_nar.html #investigation_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #investigation');
+    $('.button_menu').load('../narratives/ranger_nar.txt #investigation_menu');
     $('.button_menu').on("click", "#leave_alone", chrgResumePath);
     $('.button_menu').on("click", "#check_further", checkNoiseFurther);
 }
 
 function checkNoiseFurther() {
-    $('#autoload').load('../naratives/ranger_nar.html #check_noise');
-    $('.button_menu').load('../naratives/ranger_nar.html #check_noise_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #check_noise');
+    $('.button_menu').load('../narratives/ranger_nar.txt #check_noise_menu');
     $('.button_menu').on("click", "#poisoned", toDefeatPage);
 }
 
 function chrgAvoidCamp() {
-    $('#autoload').load('../naratives/ranger_nar.html #chrg_cmpfr_avoid');
-    $('.button_menu').load('../naratives/ranger_nar.html #chrg_cmpfr_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #chrg_cmpfr_avoid');
+    $('.button_menu').load('../narratives/ranger_nar.txt #chrg_cmpfr_menu');
     $('.button_menu').on("click", "#chrg_continue", toRngrBossPage);
 }
 
 function chrgApprchCamp() {
-    $('#autoload').load('../naratives/ranger_nar.html #chrg_cmpfr_apprch');
-    $('.button_menu').load('../naratives/ranger_nar.html #chrg_cmpfr_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #chrg_cmpfr_apprch');
+    $('.button_menu').load('../narratives/ranger_nar.txt #chrg_cmpfr_menu');
     $('.button_menu').on("click", "#chrg_continue", toRngrBossPage);
 }
 
 // sense path functions are denoted with "snse"
 function loadSense() {
-    $('#autoload').load('../naratives/ranger_nar.html #sense');
-    $('.button_menu').load('../naratives/ranger_nar.html #sense_menu_nrml');
+    $('#autoload').load('../narratives/ranger_nar.txt #sense');
+    $('.button_menu').load('../narratives/ranger_nar.txt #sense_menu_nrml');
     $('.button_menu').on("click", "#nrml_left_path_bttn", snseTakeLeftPath);
     $('.button_menu').on("click", "#nrml_midl_path_bttn", snseTakeMiddlePath);
     $('.button_menu').on("click", "#nrml_rght_path_bttn", snseTakeRightPath);
@@ -68,13 +68,13 @@ function snseTakeLeftPath() {
     // get_bait = true;
     // console.log("do you have bait? :: " + have_bait);
     if (get_bait = false)  {
-        $('#autoload').load('../naratives/ranger_nar.html #left_path');
+        $('#autoload').load('../narratives/ranger_nar.txt #left_path');
         get_bait = true;
     }
     if (get_bait = true) {
-        $('#autoload').load('../naratives/ranger_nar.html #left_have_bait');
+        $('#autoload').load('../narratives/ranger_nar.txt #left_have_bait');
     }
-    $('.button_menu').load('../naratives/ranger_nar.html #left_path_menu');
+    $('.button_menu').load('../narratives/ranger_nar.txt #left_path_menu');
     $('.button_menu').on("click", "#left_return_bttn", snseLeftReturn);
     sessionStorage.setItem("bait_key", get_bait);
     console.log("have bait? :: " + sessionStorage.getItem("bait_key"));
@@ -83,9 +83,9 @@ function snseTakeLeftPath() {
 function snseLeftReturn() {
     // left_disabled = true;
     // console.log("left path disabled? :: " + left_disabled);
-    $('#autoload').load('../naratives/ranger_nar.html #left_return');
-    $('.button_menu').load('../naratives/ranger_nar.html #sense_menu_left_dsbl');
-    // $('.button_menu').load('../naratives/ranger_nar.html #sense_menu_left_dsbl');
+    $('#autoload').load('../narratives/ranger_nar.txt #left_return');
+    $('.button_menu').load('../narratives/ranger_nar.txt #sense_menu_left_dsbl');
+    // $('.button_menu').load('../narratives/ranger_nar.txt #sense_menu_left_dsbl');
     // $('.button_menu').find('#left_path_bttn').prop("disabled", true);
     // $('.button_menu').on("click", "#left_path_bttn", function() {console.log('why you click me?')});
     $('.button_menu').on("click", "#lfds_midl_path_bttn", snseTakeMiddlePath);
@@ -94,47 +94,47 @@ function snseLeftReturn() {
 
 function snseTakeMiddlePath() {
     console.log("middle path taken");
-    $('#autoload').load('../naratives/ranger_nar.html #midl_path');
-    $('.button_menu').load('../naratives/ranger_nar.html #midl_path_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #midl_path');
+    $('.button_menu').load('../narratives/ranger_nar.txt #midl_path_menu');
     $('.button_menu').on("click", "#glint_ignore", snseGlintIgnore);
     $('.button_menu').on("click", "#glint_crouch", snseCrouch);
 }
 
 function snseGlintIgnore() {
-    $('#autoload').load('../naratives/ranger_nar.html #ignored');
-    $('.button_menu').load('../naratives/ranger_nar.html #ignored_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #ignored');
+    $('.button_menu').load('../narratives/ranger_nar.txt #ignored_menu');
     $('.button_menu').on("click", "#arrow_continue", toDefeatPage);
 }
 
 function snseCrouch() {
-    $('#autoload').load('../naratives/ranger_nar.html #crouched');
-    $('.button_menu').load('../naratives/ranger_nar.html #crouched_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #crouched');
+    $('.button_menu').load('../narratives/ranger_nar.txt #crouched_menu');
     $('.button_menu').on("click", "#crouch_continue", snseMidPathCont);
 }
 
 function snseMidPathCont() {
-    $('#autoload').load('../naratives/ranger_nar.html #midl_cont');
-    $('.button_menu').load('../naratives/ranger_nar.html #midl_cont_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #midl_cont');
+    $('.button_menu').load('../narratives/ranger_nar.txt #midl_cont_menu');
     $('.button_menu').on("click", "#snse_avoid", snseCampAvoid);
     $('.button_menu').on("click", "#snse_apprch", snseCampApprch);
 }
 
 function snseCampAvoid() {
-    $('#autoload').load('../naratives/ranger_nar.html #snse_camp_alt');
-    $('.button_menu').load('../naratives/ranger_nar.html #snse_camp_continue_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #snse_camp_alt');
+    $('.button_menu').load('../narratives/ranger_nar.txt #snse_camp_continue_menu');
     $('.button_menu').on("click", "#snse_camp_continue", toRngrBossPage);
 }
 
 function snseCampApprch() {
-    $('#autoload').load('../naratives/ranger_nar.html #snse_camp_apprch');
-    $('.button_menu').load('../naratives/ranger_nar.html #snse_camp_continue_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #snse_camp_apprch');
+    $('.button_menu').load('../narratives/ranger_nar.txt #snse_camp_continue_menu');
     $('.button_menu').on("click", "#snse_camp_continue", toRngrBossPage);
 }
 
 function snseTakeRightPath() {
     console.log("do you have treasure? :: " + get_treasure);
-    $('#autoload').load('../naratives/ranger_nar.html #rght_path');
-    $('.button_menu').load('../naratives/ranger_nar.html #rght_path_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #rght_path');
+    $('.button_menu').load('../narratives/ranger_nar.txt #rght_path_menu');
     // $('.button_menu').on("click", "#rght_return_bttn", snseRghtReturn);
     $('.button_menu').on("click", "#take_trsr_bttn", snseTakeTreasure);
     $('.button_menu').on("click", "#leave_trsr_bttn", snseLeaveTreasure);
@@ -143,8 +143,8 @@ function snseTakeRightPath() {
 function snseTakeTreasure() {
     get_treasure = true;
     sessionStorage.setItem("treasure_key", get_treasure);
-    $('#autoload').load('../naratives/ranger_nar.html #take_trsr');
-    $('.button_menu').load('../naratives/ranger_nar.html #rght_return_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #take_trsr');
+    $('.button_menu').load('../narratives/ranger_nar.txt #rght_return_menu');
     $('.button_menu').on("click", "#rght_return_bttn", snseRghtReturn);
 }
 
@@ -152,20 +152,20 @@ function snseLeaveTreasure() {
     console.log("left the treasure");
     get_treasure = false;
     sessionStorage.setItem("treasure_key", get_treasure);
-    $('#autoload').load('../naratives/ranger_nar.html #leave_it');
-    $('.button_menu').load('../naratives/ranger_nar.html #rght_return_no_trsr_menu');
+    $('#autoload').load('../narratives/ranger_nar.txt #leave_it');
+    $('.button_menu').load('../narratives/ranger_nar.txt #rght_return_no_trsr_menu');
     $('.button_menu').on("click", "#rght_return_no_trsr_bttn", snseRghtReturn);
 }
 
 function snseRghtReturn() {
     console.log("got treasure? :: " + get_treasure)
-    $('#autoload').load('../naratives/ranger_nar.html #rght_return');
-    $('.button_menu').load('../naratives/ranger_nar.html #sense_menu_rght_dsbl');
+    $('#autoload').load('../narratives/ranger_nar.txt #rght_return');
+    $('.button_menu').load('../narratives/ranger_nar.txt #sense_menu_rght_dsbl');
     $('.button_menu').on("click", "#rtds_left_path_bttn", snseTakeLeftPath);
     $('.button_menu').on("click", "#rtds_midl_path_bttn", snseTakeMiddlePath);
     // $('.button_menu').on("click", "#rght_path_bttn", snseTakeRightPath);
 }
 
-function toRngrBossPage() { window.location.href = "./ranger_boss.html"; }
-function toDefeatPage() { window.location.href = "./defeat.html"; }
-function toVictoryPage() { window.location.href = "./victory.html"; }
+function toRngrBossPage() { window.location.href = "./ranger_boss.txt"; }
+function toDefeatPage() { window.location.href = "./defeat.txt"; }
+function toVictoryPage() { window.location.href = "./victory.txt"; }
