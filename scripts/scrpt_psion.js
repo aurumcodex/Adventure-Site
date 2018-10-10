8c7474b394de782120ddcbe0d01a1534c089ccbf
aurumcodex <syberfox.64@gmail.com>
2018-10-09 23:16:11 -0400

    finished warrior path; started psion; added lang declaration to all html

--- scripts/scrpt_psion.js
+++ scripts/scrpt_psion.js
@@ -1,1 +1,6 @@
 /* Author: Nathan Adams */
+
+$(document).ready(function() {
+    $('#autoload').load('../narratives/psion_nar.html #init');
+    $('.button_menu').load('../narratives/psion_nar.html #init_menu');
+});
--- scripts/scrpt_warrior.js
+++ scripts/scrpt_warrior.js
@@ -3,10 +3,15 @@
 var get_skunk = false;
 
 $(document).ready(function() {
+    // sessionStorage.setItem("skunk_key", get_skunk);
     $('#autoload').load('../narratives/warrior_nar.html #init');
     $('.button_menu').load('../narratives/warrior_nar.html #init_menu');
     $('.button_menu').on("click", "#common_bttn", takeCommon);
     $('.button_menu').on("click", "#decrepit_bttn", takeDecrepit);
+
+    $('#bossload').load('../narratives/warrior_nar.html #boss_init');
+    $('.boss_button_menu').load('../narratives/warrior_nar.html #boss_init_menu');
+    console.log("got skunk? :: " + sessionStorage.getItem("skunk_key"));
 });
 
 // common path functions are denoted w/ cmmn
@@ -18,16 +23,30 @@ function takeCommon() {
 }
 
 function cmmnFollowTracks() {
-    $('#autoload').load('../narratives/warrior_nar.html #common_path');
-    $('.button_menu').load('../narratives/warrior_nar.html #common_path_menu');
-    $('.button_menu').on("click", "#follow_tracks", cmmnFollowTracks);
-    $('.button_menu').on("click", "#ignore_tracks", cmmnIgnoreTracks);
+    $('#autoload').load('../narratives/warrior_nar.html #followed_tracks');
+    $('.button_menu').load('../narratives/warrior_nar.html #followed_tracks_menu');
+    $('.button_menu').on("click", "#cont_tracks", cmmnMeetSkunk);
+    // $('.button_menu').on("click", "#ignore_tracks", cmmnIgnoreTracks);
+}
+
+function cmmnMeetSkunk() {
+    get_skunk = true;
+    sessionStorage.setItem("skunk_key", get_skunk);
+    $('#autoload').load('../narratives/warrior_nar.html #injured_skunk');
+    $('.button_menu').load('../narratives/warrior_nar.html #inj_skunk_menu');
+    $('.button_menu').on("click", "#skunk_continue", cmmnReturnPath);
+}
+
+function cmmnReturnPath() {
+    $('#autoload').load('../narratives/warrior_nar.html #return_to_path');
+    $('.button_menu').load('../narratives/warrior_nar.html #path_return_menu');
+    $('.button_menu').on("click", "#crossroads_bttn", toCrossroads);
 }
 
 function cmmnIgnoreTracks() {
     $('#autoload').load('../narratives/warrior_nar.html #ignored_tracks');
     $('.button_menu').load('../narratives/warrior_nar.html #path_return_menu');
-    $('.button_menu').on("click", "#follow_tracks", toCrossroads);
+    $('.button_menu').on("click", "#crossroads_bttn", toCrossroads);
     // $('.button_menu').on("click", "#ignore_tracks", cmmnIgnoreTracks);
 }
 
@@ -51,11 +70,33 @@ function dcrpApprchGlow() {
     $('.button_menu').on("click", "#get_frosted", toDefeatPage);
 }
 
-// crossroad functions are denoted w/ xrd
+// crossroad functions are denoted by direction
 function toCrossroads() {
-
+    $('#autoload').load('../narratives/warrior_nar.html #crossroads');
+    $('.button_menu').load('../narratives/warrior_nar.html #crossroads_menu');
+    $('.button_menu').on("click", "#north_bttn", North);
+    $('.button_menu').on("click", "#east_bttn", East);
+    $('.button_menu').on("click", "#south_bttn", South);
 }
 
-function toWarrBossPage() { window.location.href = "./warrior_boss.txt"; }
-function toDefeatPage() { window.location.href = "./defeat.txt"; }
-function toVictoryPage() { window.location.href = "./victory.txt"; }
+function North() {
+    $('#autoload').load('../narratives/warrior_nar.html #north_menu');
+    $('.button_menu').load('../narratives/warrior_nar.html #north_menu');
+    $('.button_menu').on("click", "#north_return", toCrossroads);
+}
+
+function South() {
+    $('#autoload').load('../narratives/warrior_nar.html #south_cottage');
+    $('.button_menu').load('../narratives/warrior_nar.html #south_menu');
+    $('.button_menu').on("click", "#south_return", toCrossroads);
+}
+
+function East() {
+    $('#autoload').load('../narratives/warrior_nar.html #east_ruins');
+    $('.button_menu').load('../narratives/warrior_nar.html #east_menu');
+    $('.button_menu').on("click", "#east_cont", toWarrBossPage);
+}
+
+function toWarrBossPage() { window.location.href = "./warrior_boss.html"; }
+function toDefeatPage() { window.location.href = "./defeat.html"; }
+function toVictoryPage() { window.location.href = "./victory.html"; }
